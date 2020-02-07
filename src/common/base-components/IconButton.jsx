10b77151e27588f030936a61258697dd59340b5d
guoxiang915 <guoxiang915@hotmail.com>
2020-02-08 00:41:11 +0800

    Add IconButton component

---
+++ src/common/base-components/IconButton.jsx
@@ -0,0 +1,38 @@
+import React, { PureComponent } from "react";
+import clsx from "clsx";
+import { IconButton as MUIIconButton, withStyles } from "@material-ui/core";
+
+const styleSheet = theme => ({
+  root: {
+    // ...theme.buttons.default,
+    flex: 0,
+
+    "&:hover": {}
+  },
+  disabled: {
+    // ...theme.buttons.disabled
+  },
+  medium: {
+    padding: 12,
+    fontSize: "1.5rem"
+  },
+  small: {
+    padding: 3,
+    fontSize: "1.125rem"
+  },
+  // ...theme.backgroundColors,
+  // ...theme.textColors
+});
+
+const IconButton = ({ children, classes, size, ...props }) => (
+  <MUIIconButton
+    classes={{
+      root: clsx(classes[size] || classes.medium, classes.root)
+    }}
+    {...props}
+  >
+    {children}
+  </MUIIconButton>
+);
+
+export default withStyles(styleSheet, { name: "IconButton" })(IconButton);
--- src/common/base-components/index.js
+++ src/common/base-components/index.js
@@ -1,6 +1,7 @@
 export * from "./Box";
 export * from "./Selection";
 export { default as Button } from "./Button";
+export { default as IconButton } from "./IconButton";
 export { default as Link } from "./Link";
 export { default as Select } from "./Select";
 export { default as TextField } from "./TextField";
