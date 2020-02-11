4a9c1fc3d013bdbfddbbd949f37aa81b87e009b1
guoxiang915 <guoxiang915@hotmail.com>
2020-02-12 04:53:34 +0800

    Add Carousel component and offices list on home page

---
+++ src/common/mock/officeMockData.js
@@ -0,0 +1,80 @@
+import gallery1 from "../../assets/img/img_gallery_01@2x.png";
+
+export const offices = [
+  {
+    title: "Title (or Short Description)",
+    rating: 3.5,
+    price: 4500,
+    images: [
+      {
+        image: gallery1,
+        location: "Location"
+      },
+      {
+        image: gallery1,
+        location: "Location"
+      },
+      {
+        image: gallery1,
+        location: "Location"
+      }
+    ]
+  },
+  {
+    title: "Title (or Short Description)",
+    rating: 3.5,
+    price: 4500,
+    images: [
+      {
+        image: gallery1,
+        location: "Location"
+      },
+      {
+        image: gallery1,
+        location: "Location"
+      },
+      {
+        image: gallery1,
+        location: "Location"
+      }
+    ]
+  },
+  {
+    title: "Title (or Short Description)",
+    rating: 3.5,
+    price: 4500,
+    images: [
+      {
+        image: gallery1,
+        location: "Location"
+      },
+      {
+        image: gallery1,
+        location: "Location"
+      },
+      {
+        image: gallery1,
+        location: "Location"
+      }
+    ]
+  },
+  {
+    title: "Title (or Short Description)",
+    rating: 3.5,
+    price: 4500,
+    images: [
+      {
+        image: gallery1,
+        location: "Location"
+      },
+      {
+        image: gallery1,
+        location: "Location"
+      },
+      {
+        image: gallery1,
+        location: "Location"
+      }
+    ]
+  }
+];
