e184371588ac7ab6dff0784400c4295c5202961e
guoxiang915 <guoxiang915@hotmail.com>
2020-02-13 01:52:45 +0800

    Fix UI of home page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -339,8 +339,8 @@ const styleSheet = theme => ({
   },
 
   contactInfoWrapper: {
-    paddingTop: 90,
-    paddingBottom: 52
+    paddingTop: 58,
+    paddingBottom: 20
   }
 });
 
@@ -749,39 +749,53 @@ class Home extends Component {
         {/* Following block */}
         <Column
           classes={{ box: clsx(classes.fixedWith) }}
-          paddingTopDouble
-          paddingBottomDouble
+          paddingTop
+          paddingBottom
         >
-          <Row fullWidth>
-            <Column>
-              <Typography>{t("receiveNewsletter")}</Typography>
-              <Row paddingTopHalf fullWidth>
-                <TextField
-                  variant="outlined"
-                  placeholder={t("yourEmailAddress")}
-                  fullWidth
-                />
-              </Row>
-            </Column>
-            <Stretch />
-            <Column>
-              <Typography>{t("followUpSocials")}</Typography>
-              <Row paddingTopHalf classes={{ box: classes.socialIconsWrapper }}>
-                <Box paddingLeft paddingRight>
-                  <Twitter fontSize="large" />
-                </Box>
-                <Box paddingLeft paddingRight>
-                  <Facebook fontSize="large" />
-                </Box>
-                <Box paddingLeft paddingRight>
-                  <Instagram fontSize="large" />
-                </Box>
-                <Box paddingLeft paddingRight>
-                  <LinkedIn fontSize="large" />
-                </Box>
-              </Row>
-            </Column>
-          </Row>
+          <Grid container justify="space-between">
+            <Grid item xs={12} sm={6}>
+              <Column paddingTop paddingBottom>
+                <Typography>{t("receiveNewsletter")}</Typography>
+                <Row paddingTopHalf fullWidth>
+                  <TextField
+                    variant="outlined"
+                    placeholder={t("yourEmailAddress")}
+                    fullWidth
+                  />
+                </Row>
+              </Column>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Column paddingTop paddingBottom>
+                <Typography>{t("followUpSocials")}</Typography>
+                <Row
+                  paddingTopHalf
+                  classes={{ box: classes.socialIconsWrapper }}
+                >
+                  <Box paddingLeft paddingRight>
+                    <Link to="#">
+                      <Twitter fontSize="large" />
+                    </Link>
+                  </Box>
+                  <Box paddingLeft paddingRight>
+                    <Link to="#">
+                      <Facebook fontSize="large" />
+                    </Link>
+                  </Box>
+                  <Box paddingLeft paddingRight>
+                    <Link to="#">
+                      <Instagram fontSize="large" />
+                    </Link>
+                  </Box>
+                  <Box paddingLeft paddingRight>
+                    <Link to="#">
+                      <LinkedIn fontSize="large" />
+                    </Link>
+                  </Box>
+                </Row>
+              </Column>
+            </Grid>
+          </Grid>
         </Column>
 
         {/* Contact info block */}
@@ -794,67 +808,77 @@ class Home extends Component {
             className={classes.contactInfoWrapper}
           >
             <Grid item xs={6} sm={3}>
-              <Typography variant="h6">{t("rentglobal")}</Typography>
-              <Column paddingTop alignChildrenStart>
-                <Link to="/about">
-                  <Typography variant="body1">{t("aboutUs")}</Typography>
-                </Link>
-                <Link to="/news">
-                  <Typography variant="body1">{t("news")}</Typography>
-                </Link>
-                <Link to="/careers">
-                  <Typography variant="body1">{t("careers")}</Typography>
-                </Link>
-                <Link to="/contact">
-                  <Typography variant="body1">{t("contactUs")}</Typography>
-                </Link>
+              <Column paddingTopDouble paddingBottomDouble alignChildrenStart>
+                <Typography variant="h6">{t("rentglobal")}</Typography>
+                <Column paddingTop alignChildrenStart>
+                  <Link to="/">
+                    <Typography variant="body1">{t("aboutUs")}</Typography>
+                  </Link>
+                  <Link to="/">
+                    <Typography variant="body1">{t("news")}</Typography>
+                  </Link>
+                  <Link to="/">
+                    <Typography variant="body1">{t("careers")}</Typography>
+                  </Link>
+                  <Link to="/">
+                    <Typography variant="body1">{t("contactUs")}</Typography>
+                  </Link>
+                </Column>
               </Column>
             </Grid>
             <Grid item xs={6} sm={3}>
-              <Typography variant="h6">{t("discover")}</Typography>
-              <Column paddingTop alignChildrenStart>
-                <Link to="/turtorial">
-                  <Typography variant="body1">{t("howItWorks")}</Typography>
-                </Link>
-                <Link to="/legal-notice">
-                  <Typography variant="body1">{t("legalNotice")}</Typography>
-                </Link>
-                <Link to="/policy">
-                  <Typography variant="body1">{t("privacyPolicy")}</Typography>
-                </Link>
-                <Link to="/terms">
+              <Column paddingTopDouble paddingBottomDouble alignChildrenStart>
+                <Typography variant="h6">{t("discover")}</Typography>
+                <Column paddingTop alignChildrenStart>
+                  <Link to="/">
+                    <Typography variant="body1">{t("howItWorks")}</Typography>
+                  </Link>
+                  <Link to="/">
+                    <Typography variant="body1">{t("legalNotice")}</Typography>
+                  </Link>
+                  <Link to="/">
+                    <Typography variant="body1">
+                      {t("privacyPolicy")}
+                    </Typography>
+                  </Link>
+                  <Link to="/">
+                    <Typography variant="body1">
+                      {t("termsAndConditions")}
+                    </Typography>
+                  </Link>
+                </Column>
+              </Column>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Column paddingTopDouble paddingBottomDouble alignChildrenStart>
+                <Typography variant="h6">{t("support")}</Typography>
+                <Column paddingTop alignChildrenStart>
+                  <Link to="/">
+                    <Typography variant="body1">{t("help")}</Typography>
+                  </Link>
+                  <Link to="/auth/login">
+                    <Typography variant="body1">{t("login")}</Typography>
+                  </Link>
+                  <Link to="/auth/register">
+                    <Typography variant="body1">{t("register")}</Typography>
+                  </Link>
+                  <Link to="/">
+                    <Typography variant="body1">{t("support")}</Typography>
+                  </Link>
+                </Column>
+              </Column>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Column paddingTopDouble paddingBottomDouble alignChildrenStart>
+                <Typography variant="h6">{t("contact")}</Typography>
+                <Column paddingTop alignChildrenStart>
                   <Typography variant="body1">
-                    {t("termsAndConditions")}
+                    1176, street Bishop, Montreal,
                   </Typography>
-                </Link>
-              </Column>
-            </Grid>
-            <Grid item xs={6} sm={3}>
-              <Typography variant="h6">{t("support")}</Typography>
-              <Column paddingTop alignChildrenStart>
-                <Link to="/help">
-                  <Typography variant="body1">{t("help")}</Typography>
-                </Link>
-                <Link to="/auth/login">
-                  <Typography variant="body1">{t("login")}</Typography>
-                </Link>
-                <Link to="/auth/register">
-                  <Typography variant="body1">{t("register")}</Typography>
-                </Link>
-                <Link to="/support">
-                  <Typography variant="body1">{t("support")}</Typography>
-                </Link>
-              </Column>
-            </Grid>
-            <Grid item xs={6} sm={3}>
-              <Typography variant="h6">{t("contact")}</Typography>
-              <Column paddingTop alignChildrenStart>
-                <Typography variant="body1">
-                  1176, street Bishop, Montreal,
-                </Typography>
-                <Typography variant="body1">QC H3G 2E3</Typography>
-                <Typography variant="body1">info@dokstation.ca</Typography>
-                <Typography variant="body1">Phone: (514) 461-3030</Typography>
+                  <Typography variant="body1">QC H3G 2E3</Typography>
+                  <Typography variant="body1">info@dokstation.ca</Typography>
+                  <Typography variant="body1">Phone: (514) 461-3030</Typography>
+                </Column>
               </Column>
             </Grid>
           </Grid>
