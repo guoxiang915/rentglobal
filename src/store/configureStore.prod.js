009b9300666bedbfe38ca5a8b8978ff8e93386b9
guoxiang915 <guoxiang915@hotmail.com>
2020-02-23 23:40:43 +0800

    Fix configureStore for prod env

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,22 +1,31 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import createSagaMiddleWare from 'redux-saga';
-import rootReducer from '../reducers'
-import rootSaga from '../sagas'
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleWare from "redux-saga";
+import rootReducer from "../reducers";
+import rootSaga from "../sagas";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
 
 const sagaMiddleWare = createSagaMiddleWare();
+const history = createBrowserHistory();
 
 const configureStore = initialState => {
-    const middlewares = [
-        sagaMiddleWare,
-    ];
-    const store = createStore(
-        rootReducer,
-        initialState,
-        compose(applyMiddleware(...middlewares))
-    );
-    
-     sagaMiddleWare.run(rootSaga);
-     return store;
-}
+  const middlewares = [sagaMiddleWare, routerMiddleware(history)];
+  const store = createStore(
+    connectRouter(history)(rootReducer),
+    initialState,
+    compose(applyMiddleware(...middlewares))
+  );
+
+  if (module.hot) {
+    //Enable Webpack hot module replacement from reducers
+    module.hot.accept("../reducers", () => {
+      const nextReducer = connectRouter(history)(rootReducer); // eslint-disable-line global-require
+      store.replaceReducer(nextReducer);
+    });
+  }
+
+  sagaMiddleWare.run(rootSaga);
+  return store;
+};
 
 export default configureStore;
