2f203448e2d0bc8b9c32bff9a129fc0f9c8df274
guoxiang915 <guoxiang915@hotmail.com>
2020-05-13 08:53:49 -0700

    #JRB-I230 Image gallery fix and lightbox

---
+++ src/common/base-components/FullScreenImageCarousel.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Backdrop,
+  withStyles,
+} from '@material-ui/core';
+
+const styleSheet = () => {
+  return {
+    backdrop: {
+      zIndex: 99999,
+    },
+    image: {
+      maxWidth: '100%'
+    }
+  };
+};
+
+const FullScreenImageCarousel = ({
+  classes,
+  image,
+  open,
+  onClose,
+}) => (
+  <Backdrop open={open} onClick={onClose} className={classes.backdrop}>
+    <img src={image} className={classes.image} />
+  </Backdrop>
+);
+
+export default withStyles(styleSheet, { name: 'FullScreenImageCarousel' })(FullScreenImageCarousel);
