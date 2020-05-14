4b603f73f7b5bdf76fb5f92862acd44277e44cab
guoxiang915 <guoxiang915@hotmail.com>
2020-05-14 08:36:50 -0700

    Fix issue of FullScreenImageCarousel component

--- src/common/base-components/FullScreenImageCarousel.jsx
+++ src/common/base-components/FullScreenImageCarousel.jsx
@@ -1,60 +1,64 @@
-import React, { useState } from 'react';
-import {
-  Dialog,
-  withStyles,
-  ClickAwayListener,
-} from '@material-ui/core';
-import { ArrowBackIos, ArrowForwardIos, } from '@material-ui/icons';
-import clsx from 'clsx';
+import React, { useState } from "react";
+import { Dialog, withStyles } from "@material-ui/core";
+import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
+import clsx from "clsx";
 
 const styleSheet = () => {
   return {
     dialog: {
-      maxWidth: 'calc(100% - 64px)'
+      maxWidth: "calc(100% - 64px)",
     },
     image: {
-      maxWidth: '100%'
+      maxWidth: "100%",
     },
-    imageWrapper: {
-    },
+    imageWrapper: {},
     navButton: {
-      position: 'absolute',
-      top: '50%',
-      transform: 'translateY(-50%)',
-      border: 'none',
-      backgroundColor: 'transparent',
-      cursor: 'pointer',
-      color: '#b9b9b9',
-      transition: '0.5s',
-      outline: 'none',
+      position: "absolute",
+      top: "50%",
+      transform: "translateY(-50%)",
+      border: "none",
+      backgroundColor: "transparent",
+      cursor: "pointer",
+      color: "#b9b9b9",
+      transition: "0.5s",
+      outline: "none",
 
-      '&:hover': {
-        color: 'black'
-      }
+      "&:hover": {
+        color: "black",
+      },
     },
     navBackButton: {},
     navForwardButton: {
-      right: 0
-    }
+      right: 0,
+    },
   };
 };
 
-const FullScreenImageCarousel = ({
-  classes,
-  images,
-  index,
-  open,
-  onClose,
-}) => {
+const FullScreenImageCarousel = ({ classes, images, index, open, onClose }) => {
   const [currentPhotoIndex, setCurrentPhotoIndex] = useState(index);
   return (
     <Dialog open={open} onClose={onClose} classes={{ paper: classes.dialog }}>
-      <button onClick={() => setCurrentPhotoIndex(Math.max(0, currentPhotoIndex - 1))} className={clsx(classes.navButton, classes.navBackButton)}>
-        <ArrowBackIos fontSize="large" />
+      <button
+        onClick={() => setCurrentPhotoIndex(Math.max(0, currentPhotoIndex - 1))}
+        className={clsx(classes.navButton, classes.navBackButton)}
+      >
+        <ArrowBackIos fontSize='large' />
       </button>
-      <img src={images[currentPhotoIndex]} className={classes.image} onClick={(e) => e.preventDefault()} />
-      <button onClick={() => setCurrentPhotoIndex(Math.min(images.length - 1, currentPhotoIndex + 1))} className={clsx(classes.navButton, classes.navForwardButton)}>
-        <ArrowForwardIos fontSize="large" />
+      <img
+        src={images[currentPhotoIndex]}
+        alt=''
+        className={classes.image}
+        onClick={(e) => e.preventDefault()}
+      />
+      <button
+        onClick={() =>
+          setCurrentPhotoIndex(
+            Math.min(images.length - 1, currentPhotoIndex + 1)
+          )
+        }
+        className={clsx(classes.navButton, classes.navForwardButton)}
+      >
+        <ArrowForwardIos fontSize='large' />
       </button>
     </Dialog>
   );
