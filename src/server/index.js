707818cba079725e6fee9f384d2c811a53fa88a6
guoxiang915 <guoxiang915@hotmail.com>
2020-05-18 13:52:15 -0700

    #JRB-I159 social header user agent catch

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,12 @@ import renderMiddleware from './middleware/render';
 const publicPath = path.join(__dirname, '/public');
 const app = express();
 
+app.use((req, res, next) => {
+  const userAgent = req.headers['user-agent'];
+  if (/^(facebookexternalhit)|(Twitterbot)|(Pinterest)/gi.test(userAgent)) { console.log(userAgent, 'is a bot'); }
+  next();
+});
+
 app.use(express.static(publicPath));
 app.use(htmlMiddleware());
 app.use(storeMiddleware());
