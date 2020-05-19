18c4c4bd170b6a44cc23c490cecbcd1b8b73f661
guoxiang915 <guoxiang915@hotmail.com>
2020-05-19 02:20:41 -0700

    #JRB-I236 meta tag rendering if crawling

--- src/server/index.js
+++ src/server/index.js
@@ -4,14 +4,20 @@ import express from 'express';
 import htmlMiddleware from './middleware/html';
 import storeMiddleware from './middleware/store';
 import renderMiddleware from './middleware/render';
+import nonSpaRouter from './routes/nonSpaRoutes';
 
 const publicPath = path.join(__dirname, '/public');
 const app = express();
 
+app.set('view engine', 'jade');
+
 app.use((req, res, next) => {
   const userAgent = req.headers['user-agent'];
-  if (/^(facebookexternalhit)|(Twitterbot)|(Pinterest)/gi.test(userAgent)) { console.log(userAgent, 'is a bot'); }
-  next();
+  if (/^(facebookexternalhit)|(Twitterbot)|(Pinterest)/gi.test(userAgent)) {
+    nonSpaRouter(req, res, next);
+  } else {
+    next();
+  }
 });
 
 app.use(express.static(publicPath));
