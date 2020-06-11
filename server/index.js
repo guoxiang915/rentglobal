8205fc6f6a868163027233390e4326ce140a4369
guoxiang915 <guoxiang915@hotmail.com>
2020-06-10 18:47:43 -0700

    Config port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const path = require('path');
 const fs = require('fs');
 
