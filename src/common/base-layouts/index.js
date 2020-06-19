af98e7db01bbcc9f7685d0d511fc327e90ae2531
guoxiang915 <guoxiang915@hotmail.com>
2020-06-19 08:22:01 -0700

    Landlord calendar accepted visit requests page

--- src/common/base-layouts/index.js
+++ src/common/base-layouts/index.js
@@ -3,6 +3,7 @@ export * from "./StatisticBox";
 export { default as TabWrapper } from "./TabWrapper";
 export { default as OfficeItem } from "./OfficeItem";
 export { default as VisitRequestItem } from "./VisitRequestItem";
+export { default as AcceptedVisitRequestItem } from "./AcceptedVisitRequestItem";
 export { default as OfficeListItem } from "./OfficeListItem";
 export { default as OfficeDetailItem } from "./OfficeDetailItem";
 export { default as OfficeTitlebar } from "./OfficeTitlebar";
